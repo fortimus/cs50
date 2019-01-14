f6571d3656be4fdda73c6e829fcd935014deaecb
Michael Fort <fortimusmac@Michaels-MacBook-Pro.local>
2019-01-13 22:16:49 -0500

    -amend

---
+++ samples/collatz.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    int n;
+    
+    do
+    {
+        printf("Integer: ");
+        scanf("%i", &n);
+    }
+    while (n < 1);
+
+    printf("Steps: %i\n", collatz(n));
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n % 2 == 0)
+    {
+        return 1 + collatz(n / 2);
+    }
+    else
+    {
+        return 1 + collatz(3 * n + 1);
+    }
+}
